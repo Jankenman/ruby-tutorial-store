d338931bc3474ce4b326cc97cfcd4d854678ae01
Jankenman <120563461+Jankenman@users.noreply.github.com>
2026-01-13 20:30:26 +0900

    Render + Supabase にデプロイできるように設定

---
+++ bin/render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# エラーが発生したら即座に終了
+set -o errexit
+
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
+# データベースのマイグレーション
+bundle exec rails db:migrate
